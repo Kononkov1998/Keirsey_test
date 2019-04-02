e043c97dfb3fde1629d711ddf643c5320b342643
kostya007 <kostya.1998.nosarev@mail.ru>
2019-04-03 00:54:56 +0700

    Оформление окна результата теста

--- app/src/main/java/com/example/wav9/caers/ResultActivity.java
+++ app/src/main/java/com/example/wav9/caers/ResultActivity.java
@@ -11,6 +11,12 @@ public class ResultActivity extends AppCompatActivity {
 
     private Counter counter;
 
+    private String result;
+
+    private TextView tvResultName;
+
+    private TextView tvResultDisc;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,45 +24,86 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
 
         counter = getIntent().getParcelableExtra("counter");
-        TextView result = findViewById(R.id.textViewResult);
-        result.setText(findResultText());
+        result = calculateResult();
+
+        tvResultName = findViewById(R.id.result_name_textView);
+        String resultNameString = getResources().getString(R.string.your_type) + getResources().getString(getResultName());
+        tvResultName.setText(resultNameString);
+
+        tvResultDisc = findViewById(R.id.result_disc_textView);
+        tvResultDisc.setText(getResultDiscription());
     }
 
-    private int findResultText(){
-        String result = calculateResult();
+    private int getResultName() {
+        if (result.equals("ESFP"))
+            return R.string.ESFP_name;
+        else if (result.equals("ISFP"))
+            return R.string.ISFP_name;
+        else if (result.equals("ESTP"))
+            return R.string.ESTP_name;
+        else if (result.equals("ISTP"))
+            return R.string.ISTP_name;
+        else if (result.equals("ESFJ"))
+            return R.string.ESFJ_name;
+        else if (result.equals("ISFJ"))
+            return R.string.ISFJ_name;
+        else if (result.equals("ESTJ"))
+            return R.string.ESTJ_name;
+        else if (result.equals("ISTJ"))
+            return R.string.ISTJ_name;
+        else if (result.equals("ENFJ"))
+            return R.string.ENFJ_name;
+        else if (result.equals("INFJ"))
+            return R.string.INFJ_name;
+        else if (result.equals("ENFP"))
+            return R.string.ENFP_name;
+        else if (result.equals("INFP"))
+            return R.string.INFP_name;
+        else if (result.equals("ENTJ"))
+            return R.string.ENTJ_name;
+        else if (result.equals("INTJ"))
+            return R.string.INTJ_name;
+        else if (result.equals("ENTP"))
+            return R.string.ENTP_name;
+        else if (result.equals("INTP"))
+            return R.string.INTP_name;
+        else return 0;
+    }
+
+    private int getResultDiscription() {
 
         if (result.equals("ESFP"))
-            return R.string.ESFP;
+            return R.string.ESFP_disc;
         else if (result.equals("ISFP"))
-            return R.string.ISFP;
+            return R.string.ISFP_disc;
         else if (result.equals("ESTP"))
-            return R.string.ESTP;
+            return R.string.ESTP_disc;
         else if (result.equals("ISTP"))
-            return R.string.ISTP;
+            return R.string.ISTP_disc;
         else if (result.equals("ESFJ"))
-            return R.string.ESFJ;
+            return R.string.ESFJ_disc;
         else if (result.equals("ISFJ"))
-            return R.string.ISFJ;
+            return R.string.ISFJ_disc;
         else if (result.equals("ESTJ"))
-            return R.string.ESTJ;
+            return R.string.ESTJ_disc;
         else if (result.equals("ISTJ"))
-            return R.string.ISTJ;
+            return R.string.ISTJ_disc;
         else if (result.equals("ENFJ"))
-            return R.string.ENFJ;
+            return R.string.ENFJ_disc;
         else if (result.equals("INFJ"))
-            return R.string.INFJ;
+            return R.string.INFJ_disc;
         else if (result.equals("ENFP"))
-            return R.string.ENFP;
+            return R.string.ENFP_disc;
         else if (result.equals("INFP"))
-            return R.string.INFP;
+            return R.string.INFP_disc;
         else if (result.equals("ENTJ"))
-            return R.string.ENTJ;
+            return R.string.ENTJ_disc;
         else if (result.equals("INTJ"))
-            return R.string.INTJ;
+            return R.string.INTJ_disc;
         else if (result.equals("ENTP"))
-            return R.string.ENTP;
+            return R.string.ENTP_disc;
         else if (result.equals("INTP"))
-            return R.string.INTP;
+            return R.string.INTP_disc;
         else return 0;
     }
 
